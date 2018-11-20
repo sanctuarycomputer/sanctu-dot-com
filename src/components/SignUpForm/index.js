f8d9d0b656a9bf8b0b2b385070ab71aa28dd7d4e
Dan <danhemerlein@gmail.com>
2018-11-20 18:15:05 -0500

    implement signup form

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -4,47 +4,83 @@ import "what-input";
 
 import "./SignUpForm.scss";
 
+import ENV from 'config/Environment';
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
+
 class SignUpForm extends PureComponent {
   constructor(props) {
     super(...arguments);
 
     this.state = {
-      value: ""
+      emailAddress: '',
+      message: '',
     };
   }
 
   handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
+    this.setState({ emailAddress: event.target.value });
   };
 
   render() {
+    const url = ENV.MAILCHIMP_URL;
+    const body = document.querySelector('body');
+
+    body.addEventListener('keyup', function (event) {
+      event.preventDefault();
+      if (event.keyCode === 13) {
+        const input = document.getElementById("submit")
+        input.click();
+      }
+    });
+
     return (
       <div className={"SignUpForm"}>
-        <form onSubmit={this.handleSubmit}>
-          <label className="small">
-            <h3>{this.props.title}</h3>
-            <input
-              className="SignUpForm__input small ml1_25 p0 w100 inline-block"
-              type="email"
-              placeholder="hello@example.com"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
-          <div>
-            <input
-              className="SignUpForm__button ml1_25 p0 small"
-              type="submit"
-              value="Submit"
-            />
+        <MailchimpSubscribe
+          url={url}
+          render={({ subscribe, status, message }) => (
+            <div>
+              <label className="small">
+                <h3>{this.props.title}</h3>
+                <input
+                  className="SignUpForm__input small ml1_25 p0 w100 inline-block"
+                  type="email"
+                  placeholder="hello@example.com"
+                  value={this.state.emailAddress}
+                  onChange={this.handleChange}
+                />
+              </label>
+            <div>
+              <input
+                id="submit"
+                className="SignUpForm__button ml1_25 p0 small"
+                type="submit"
+                value="Submit"
+                onClick={() => subscribe({ EMAIL: this.state.emailAddress })}
+              />
+              <div className="relative w100">
+                <p className="smaller absolute ml1_25 p0">{this.state.message}</p>
+              </div>
+            </div>
+            {status === 'sending' ? (
+              this.setState({
+                message: 'Loading...'
+              })
+            ) : null}
+            {status === 'success' ? (
+              this.setState({
+                message: 'Thanks for subscribing'
+              })
+            ) : null}
+            {status === 'error' ? (
+              this.setState({
+                message: `Oops! Please try again`
+              })
+            ) : null}
           </div>
-        </form>
-      </div>
-    );
+        )}
+      />
+    </div>
+    )
   }
 }
 
