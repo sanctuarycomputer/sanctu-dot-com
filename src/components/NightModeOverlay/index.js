cecdc13c547396b0412ec940db4f66ab9521ad56
Christine Kim <chrismekim@gmail.com>
2019-11-14 16:55:41 -0500

    updated css for social media placeholder links

--- src/components/NightModeOverlay/index.js
+++ src/components/NightModeOverlay/index.js
@@ -25,41 +25,40 @@ class NightModeOverlay extends PureComponent {
       className
     } = this.props;
 
+    const { isActive } = this.state
+
     return (
-      
       <div
       className={cx(
-        'NightModeOverlay absolute fixed vh100 vw100 md:w100 md:h100 flex flex-col z3 col-12 overflow-hidden',
+        'NightModeOverlay absolute fixed vh100 vw100 flex flex-col z3 col-12 overflow-hidden',
         className
       )}>
         <div className="flex flex-col md:flex-row col-12 justify-between">
-
-          <div className="flex flex-col col-1">
-            <nav className="NightModeOverlay__navigation md:block col-4 mb6">
-              <ul>Twitter</ul>
-              <ul>Instagram</ul>
-              <ul>Media</ul>
-              <ul className="none md:block">Github</ul>
-              <ul className="none md:block">Podcast</ul>
-            </nav>
-            <div className="NightModeOverlay__sign-up-form none md:block">
+          <div className="NightModeOverlay__navigation flex flex-col col-1 p1">
+            <div className="small md:block none">Social Media:</div>
+              <nav 
+                aria-label="Navigation for Sanctuary Computer Social Media Links"
+                className="NightModeOverlay__social-media-links md:block col-4 md:mb6 px1 pb1 small">
+                  <a href="www.sanctuary.computer" className="block">Twitter</a>
+                  <a href="www.sanctuary.computer" className="block">Instagram</a>
+                  <a href="www.sanctuary.computer" className="block">Medium</a>
+                  <a href="www.sanctuary.computer" className="none md:block">Github</a>
+                  <a href="www.sanctuary.computer" className="none md:block">Podcast</a>
+              </nav>
+            <div className="NightModeOverlay__sign-up-form none md:block md:mt4">
               <SignUpForm title={`Email Updates:`} />
             </div>
           </div>
-
-          <div className="NightModeOverlay__intro-section paragraph p1 md:col-4 col-12 pr6">
-            <p className="pb2">Thanks for visiting Sanctuary Computer. Our website is
-            currently closed.
+          <div className="NightModeOverlay__paragraph p1 md:col-4 col-12 pr6">
+            <p className="pb2">Thanks for visiting Sanctuary Computer. Our website is currently closed.
             </p>
             <p>Hours are Monday to Friday 7 AM - 7 PM.</p>
           </div>
-
         </div>
-
         <div className="NightModeOverlay__footer absolute fixed b0 r0 pr_5">
           <Footer />
         </div>
-    </div>
+      </div>
     )
   }
 }
