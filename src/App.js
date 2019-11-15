b129c6f96f75d41e3c332e419be796a2e6773e54
Christine Kim <chrismekim@gmail.com>
2019-11-15 13:16:55 -0500

    added logic to check time to set nightmode

--- src/App.js
+++ src/App.js
@@ -10,17 +10,32 @@ import NightModeOverlay from "components/NightModeOverlay";
 import { isNightMode } from 'state/actions/nightModeActions';
 
 class App extends Component {
+
   componentDidMount() {
+    this.setMode();
+
     console.log('%c jobs: hello@sanctuary.computer', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
   }
 
-  render() {
-    const { isNightMode } = this.props
+  setMode = () => {
+    const start =  7 * 60 + 30;
+    const end =  19 * 60 + 0;
 
+    const isDaytime = () => {
+      const now = new Date();
+      const time = now.getHours() * 60 + now.getMinutes();
+      
+      return time >= start && time < end;
+    }
+
+    if (!isDaytime()) this.props.actions.isNightMode(true);
+
+  }
+
+  render() {
     return (
       <>
-        <NightModeOverlay
-          isNightMode={isNightMode}/>
+        <NightModeOverlay/>
         <MainContainer />
       </>
     );
--- src/components/NightModeOverlay/index.js
+++ src/components/NightModeOverlay/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 // import get from "utils/get";
 // import simpleFragmentToListItems from "utils/simpleFragmentToListItems";
 
@@ -10,57 +11,58 @@ import cx from 'classnames';
 import "./NightModeOverlay.scss";
 
 class NightModeOverlay extends PureComponent {
-  constructor(props) {
-    super(...arguments);
-
-    this.state = {
-      isActive: this.props.isNightMode
-    }
-
-  };
 
   render() {
 
     const {
-      className
+      className,
+      isNightMode
     } = this.props;
 
-    const { isActive } = this.state
-
-    return (
-      <div
-      className={cx(
-        'NightModeOverlay absolute fixed vh100 vw100 flex flex-col z3 col-12 overflow-hidden',
-        className
-      )}>
-        <div className="flex flex-col md:flex-row col-12 justify-between">
-          <div className="NightModeOverlay__navigation flex flex-col col-1 p1">
-            <div className="small md:block none">Social Media:</div>
-              <nav 
-                aria-label="Navigation for Sanctuary Computer Social Media Links"
-                className="NightModeOverlay__social-media-links md:block col-4 md:mb6 px1 pb1 small">
-                  <a href="www.sanctuary.computer" className="block">Twitter</a>
-                  <a href="www.sanctuary.computer" className="block">Instagram</a>
-                  <a href="www.sanctuary.computer" className="block">Medium</a>
-                  <a href="www.sanctuary.computer" className="none md:block">Github</a>
-                  <a href="www.sanctuary.computer" className="none md:block">Podcast</a>
-              </nav>
-            <div className="NightModeOverlay__sign-up-form none md:block md:mt4">
-              <SignUpForm title={`Email Updates:`} />
+      return (
+        <>
+        { !!isNightMode && ( <div
+          className={cx(
+            'NightModeOverlay absolute fixed vh100 vw100 flex flex-col z3 col-12 overflow-hidden',
+            className
+          )}>
+            <div className="flex flex-col md:flex-row col-12 justify-between">
+              <div className="NightModeOverlay__navigation flex flex-col col-1 p1">
+                <div className="small md:block none">Social Media:</div>
+                  <nav 
+                    aria-label="Navigation for Sanctuary Computer Social Media Links"
+                    className="NightModeOverlay__social-media-links md:block col-4 md:mb6 px1 pb1 small">
+                      <a href="www.sanctuary.computer" className="block">Twitter</a>
+                      <a href="www.sanctuary.computer" className="block">Instagram</a>
+                      <a href="www.sanctuary.computer" className="block">Medium</a>
+                      <a href="www.sanctuary.computer" className="none md:block">Github</a>
+                      <a href="www.sanctuary.computer" className="none md:block">Podcast</a>
+                  </nav>
+                <div className="NightModeOverlay__sign-up-form none md:block md:mt4">
+                  <SignUpForm title={`Email Updates:`} />
+                </div>
+              </div>
+              <div className="NightModeOverlay__paragraph p1 md:col-4 col-12 pr6">
+                <p className="pb2">Thanks for visiting Sanctuary Computer. Our website is currently closed.
+                </p>
+                <p>Hours are Monday to Friday 7 AM - 7 PM.</p>
+              </div>
             </div>
-          </div>
-          <div className="NightModeOverlay__paragraph p1 md:col-4 col-12 pr6">
-            <p className="pb2">Thanks for visiting Sanctuary Computer. Our website is currently closed.
-            </p>
-            <p>Hours are Monday to Friday 7 AM - 7 PM.</p>
-          </div>
-        </div>
-        <div className="NightModeOverlay__footer absolute fixed b0 r0 pr_5">
-          <Footer />
-        </div>
-      </div>
-    )
+            <div className="NightModeOverlay__footer absolute fixed b0 r0 pr_5">
+              <Footer />
+            </div>
+          </div> )}
+          </>
+      )
+    
   }
 }
 
-export default NightModeOverlay;
+const mapStateToProps = (state) => ({
+  isNightMode: state.nightMode.isNightMode
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(NightModeOverlay);
