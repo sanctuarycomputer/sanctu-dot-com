d26e9a62f1f9404bc33416ee136be03e19bbbfaf
sohee Lim <limsohee1002@gmail.com>
2019-12-04 14:37:34 -0500

    style BlockGeneralInfo

--- src/components/base/List/index.js
+++ src/components/base/List/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import get from 'utils/get';
 
 import cx from 'classnames';
-import './List.scss';
 
 const List = ({ title, listItems, className }) => {
   const hasListItems = !!(listItems && listItems.length);
--- src/views/CaseStudyView.js
+++ src/views/CaseStudyView.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 import get from "utils/get";
 
+import CaseStudyBlockSwitch from 'components/CaseStudyBlockSwitch';
+
 const CaseStudyView = ({ model }) => {
   if (!model || model.isError || !model.sys || !model.fields) return <h1>Something went wrong...</h1>;
 
   return (
     <Fragment>
-      CaseStudyView
+      {get(model, 'fields.contentBlocks', []).map((block, i) => (
+        <CaseStudyBlockSwitch key={get(block, 'sys.id', i)} block={block}/>
+      ))}
     </Fragment>
   );
 };
