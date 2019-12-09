efcb88fb188d235374abb0d4f043deb7f3bc63db
Christine Kim <chrismekim@gmail.com>
2019-12-09 09:50:49 -0500

    add styling to overlay

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
-import SanctuLogo from "assets/sanctu_logo.svg";
+import SanctuLogoBlack from "assets/sanctu_logo_black.svg";
+import SanctuLogoWhite from "assets/sanctu_logo_white.svg";
 
-const Footer = ({ hidden }) => (
+const Footer = ({ hidden }) => (  
   <footer 
     aria-hidden={ hidden }
     className="Footer col-8 p1">
     <img
       className="Footer__icon"
-      src={SanctuLogo}
+      src={ hidden ? SanctuLogoWhite : SanctuLogoBlack }
       alt="sanctuary computer logo"
     />
   </footer>
