fa7ce1f00a8b00180ce9230b274d149894f2b30d
Christine Kim <chrismekim@gmail.com>
2020-04-20 12:03:07 -0400

    make block col-4 width

--- src/components/BlockViewMore.js
+++ src/components/BlockViewMore.js
@@ -7,6 +7,7 @@ import { ContentfulMedia } from 'models';
 import flattenImageData from 'utils/flattenImageData';
 
 import { Image } from 'components/base';
+import SanctuLogoBlack from 'assets/sanctu_logo_black.svg';
 
 const BlockViewMore = props => {
   const fields = get(props, 'block.fields');
@@ -28,45 +29,56 @@ const BlockViewMore = props => {
       }}
       className="BlockViewMore flex flex-col px1 pb1"
     >
-      {header && <span className="small pb1">{header}</span>}
-      <div className="flex flex-col md:flex-row">
-        <div className="md:col-4 flex flex-col pb1 md:pb0">
-          {linkOneText && linkOne && (
-            <Link
-              className="small link decoration-none"
-              aria-label={linkOneText}
-              to={linkOne}
-              rel="noopener noreferrer"
-            >
-              <div className="flex flex-col">
-                <Image
-                  className="h100 w100 hauto fit-cover pb1 md:pr1"
-                  alt={linkOneImage.description}
-                  src={linkOneImage.url}
-                />
-                → {linkOneText}
-              </div>
-            </Link>
-          )}
-        </div>
+      <div className="flex flex-col md:flex-row-reverse md:flex-row justify-between">
         <div className="md:col-4 flex flex-col">
-          {linkTwoText && linkTwo && (
-            <Link
-              className="small link decoration-none"
-              aria-label={linkTwoText}
-              to={linkTwo}
-              rel="noopener noreferrer"
-            >
-              <div className="flex flex-col">
-                <Image
-                  className="h100 w100 hauto fit-cover pb1"
-                  alt={linkTwoImage.description}
-                  src={linkTwoImage.url}
-                />
-                → {linkTwoText}
-              </div>
-            </Link>
-          )}
+          {header && <span className="small pb1">{header}</span>}
+          <div className="flex flex-col md:flex-row">
+            <div className="md:col-4 flex flex-col pb2 md:pb0">
+              {linkOneText && linkOne && (
+                <Link
+                  className="small link decoration-none"
+                  aria-label={linkOneText}
+                  to={linkOne}
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex flex-col">
+                    <Image
+                      className="h100 w100 hauto fit-cover pb1 md:pr1"
+                      alt={linkOneImage.description}
+                      src={linkOneImage.url}
+                    />
+                    → {linkOneText}
+                  </div>
+                </Link>
+              )}
+            </div>
+            <div className="md:col-4 flex flex-col">
+              {linkTwoText && linkTwo && (
+                <Link
+                  className="small link decoration-none"
+                  aria-label={linkTwoText}
+                  to={linkTwo}
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex flex-col">
+                    <Image
+                      className="h100 w100 hauto fit-cover pb1"
+                      alt={linkTwoImage.description}
+                      src={linkTwoImage.url}
+                    />
+                    → {linkTwoText}
+                  </div>
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="col-4 flex flex-col justify-end pt5 md:pt0">
+          <img
+            className="BlockViewMore__footer-icon"
+            src={SanctuLogoBlack}
+            alt="Sanctuary Computer logo"
+          />
         </div>
       </div>
     </div>
