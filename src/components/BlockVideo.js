8fb18a8ac56a68100766cf1ca0fa027431f97ef2
Christine Kim <chrismekim@gmail.com>
2020-04-21 16:42:15 -0400

    add option for image and video

--- src/components/BlockVideo.js
+++ src/components/BlockVideo.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 
 import get from 'utils/get';
 import cx from 'classnames';
+import flattenImageData from 'utils/flattenImageData';
 
 import { ContentfulMedia } from 'models';
+import { Image } from 'components/base';
 
 const BlockVideo = props => {
   const fields = get(props, 'block.fields');
   const video = get(fields, 'video', '');
+  const image = flattenImageData(get(fields, 'image', {}));
   const videoSize = get(props, 'block.fields.videoSize', 'Full').toLowerCase();
   const autoPlay = get(fields, 'autoPlayVideo', true);
   const loop = get(fields, 'loopVideo', true);
+  const hasTwoElements =
+    videoSize === 'two' && image.url && video.fields.file.url;
 
   return (
     <div
@@ -25,22 +30,50 @@ const BlockVideo = props => {
           videoSize === 'half-right-align'
       })}
     >
-      <div
-        className={cx({
-          'sm:col-4 sm:pl1': videoSize === 'half-left-align',
-          'sm:col-4 sm:pr1': videoSize === 'half-right-align'
-        })}
-      >
-        <video
-          className="BlockVideo__video block hauto w100"
-          autoPlay={autoPlay}
-          loop={loop}
-          muted
-          playsInline
+      {!hasTwoElements && (
+        <div
+          className={cx({
+            'sm:col-4 sm:pl1': videoSize === 'half-left-align',
+            'sm:col-4 sm:pr1': videoSize === 'half-right-align'
+          })}
         >
-          <source src={get(video, 'fields.file.url', '')}></source>
-        </video>
-      </div>
+          <video
+            className="BlockVideo__video block hauto w100"
+            autoPlay={autoPlay}
+            loop={loop}
+            muted
+            playsInline
+          >
+            <source src={get(video, 'fields.file.url', '')}></source>
+          </video>
+        </div>
+      )}
+      {hasTwoElements && (
+        <div
+          className={cx({
+            'flex flex-col md:flex-row px1': videoSize === 'two'
+          })}
+        >
+          <div className="pb1 md:pb0 md:pr1 md:col-4">
+            <video
+              className="BlockVideo__video h100 w100 fit-cover"
+              autoPlay={autoPlay}
+              loop={loop}
+              muted
+              playsInline
+            >
+              <source src={get(video, 'fields.file.url', '')}></source>
+            </video>
+          </div>
+          <div className="md:col-4">
+            <Image
+              className="BlockImage__image h100 w100"
+              alt={image.description}
+              src={image.url}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
@@ -49,6 +82,7 @@ BlockVideo.propTypes = {
   block: PropTypes.shape({
     fields: PropTypes.shape({
       video: ContentfulMedia,
+      image: ContentfulMedia,
       videoSize: PropTypes.string,
       autoPlay: PropTypes.bool,
       loop: PropTypes.bool
