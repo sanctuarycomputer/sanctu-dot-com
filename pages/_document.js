3574d78d151224e406ba8e3445555f1ad816e0e4
sohee Lim <limsohee1002@gmail.com>
2022-01-25 15:39:40 -0500

    Add minimunCache ttl

--- pages/_document.js
+++ pages/_document.js
@@ -14,8 +14,10 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon-16x16.png" />
           <link rel="shortcut icon" href="favicon-32x32.png" />
           <meta name="theme-color" content="#000000" />
-          <script async rel="dns-prefetch" src="https://www.googletagmanager.com/gtag/js?id=UA-88778470-4"></script>
+          <script async rel="prefetch" type="text/javascript" src="https://www.google-analytics.com/analytics.js"></script>
+          <script async rel="prefetch" src="https://www.googletagmanager.com/gtag/js?id=UA-88778470-4"></script>
           <script
+            defer
             dangerouslySetInnerHTML={{
               __html: `window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
@@ -28,16 +30,17 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <link defer rel="dns-prefetch" as="font" href="https://fonts.sanctuary.computer" />
+          <link defer rel="prefetch" as="font" href="https://fonts.sanctuary.computer/AustinNewsDeck/styles.css" />
           <link defer rel="stylesheet" type="text/css" href="https://fonts.sanctuary.computer/AustinNewsDeck/styles.css" />
+          <link defer rel="prefetch" as="font" href="https://fonts.sanctuary.computer/AtlasGrotesk/styles.css" />
           <link defer rel="stylesheet" type="text/css" href="https://fonts.sanctuary.computer/AtlasGrotesk/styles.css" />
-          <link defer rel="dns-prefetch" as="style" href="clippyjs/build/clippy.css" />
+          <link defer rel="prefetch" as="style" href="clippyjs/build/clippy.css" />
           <link defer rel="stylesheet" type="text/css" href="clippyjs/build/clippy.css" />
-          <link defer rel="dns-prefetch" as="style" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" />
+          <link defer rel="prefetch" as="style" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" />
           <link defer rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" />
-          <script defer rel="dns-prefetch" src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossOrigin="anonymous"></script>
-          <script defer rel="dns-prefetch" src="clippyjs/build/clippy.min.js"></script>
-          <script defer rel="dns-prefetch" src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+          <script defer rel="prefetch" src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossOrigin="anonymous"></script>
+          <script defer rel="prefetch" src="clippyjs/build/clippy.min.js"></script>
+          <script defer rel="prefetch" src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
         </body>
       </Html>
     )
