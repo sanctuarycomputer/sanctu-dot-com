92bf2d7914cc6da108ebbaac3bb6884cb904a32d
sohee Lim <limsohee1002@gmail.com>
2022-01-26 14:35:29 -0500

    move back link tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,14 +14,6 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon-16x16.png" />
           <link rel="shortcut icon" href="favicon-32x32.png" />
           <meta name="theme-color" content="#000000" />
-          <link defer rel="prefetch" as="font" href="https://fonts.sanctuary.computer/AustinNewsDeck/styles.css" />
-          <link defer rel="stylesheet" type="text/css" href="https://fonts.sanctuary.computer/AustinNewsDeck/styles.css" />
-          <link defer rel="prefetch" as="font" href="https://fonts.sanctuary.computer/AtlasGrotesk/styles.css" />
-          <link defer rel="stylesheet" type="text/css" href="https://fonts.sanctuary.computer/AtlasGrotesk/styles.css" />
-          <link defer rel="prefetch" as="style" href="clippyjs/build/clippy.css" />
-          <link defer rel="stylesheet" type="text/css" href="clippyjs/build/clippy.css" />
-          <link defer rel="prefetch" as="style" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" />
-          <link defer rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" />
           <script async rel="prefetch" type="text/javascript" src="https://www.google-analytics.com/analytics.js"></script>
           <script async rel="prefetch" src="https://www.googletagmanager.com/gtag/js?id=UA-88778470-4"></script>
           <script
@@ -38,6 +30,14 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <link defer rel="prefetch" as="font" href="https://fonts.sanctuary.computer/AustinNewsDeck/styles.css" />
+          <link defer rel="stylesheet" type="text/css" href="https://fonts.sanctuary.computer/AustinNewsDeck/styles.css" />
+          <link defer rel="prefetch" as="font" href="https://fonts.sanctuary.computer/AtlasGrotesk/styles.css" />
+          <link defer rel="stylesheet" type="text/css" href="https://fonts.sanctuary.computer/AtlasGrotesk/styles.css" />
+          <link defer rel="prefetch" as="style" href="clippyjs/build/clippy.css" />
+          <link defer rel="stylesheet" type="text/css" href="clippyjs/build/clippy.css" />
+          <link defer rel="prefetch" as="style" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" />
+          <link defer rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" />
           <NextScript defer strategy="lazyOnload" rel="prefetch" src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="></NextScript>
           <NextScript defer strategy="lazyOnload" rel="prefetch" src="clippyjs/build/clippy.min.js"></NextScript>
           <NextScript defer strategy="lazyOnload" rel="prefetch" src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></NextScript>
