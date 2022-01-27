eafd8f89dc640466aba0388fe3f73dc220f43bcd
sohee Lim <limsohee1002@gmail.com>
2022-01-27 16:58:24 -0500

    remove js

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,6 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon-16x16.png" />
           <link rel="shortcut icon" href="favicon-32x32.png" />
           <meta name="theme-color" content="#000000" />
-          <NextScript defer strategy="lazyOnload" rel="preconnect" src="https://images.ctfassets.net"></NextScript>
           <NextScript async rel="preconnect" type="text/javascript" src="https://www.google-analytics.com/analytics.js"></NextScript>
           <NextScript async rel="prefetch" src="https://www.googletagmanager.com/gtag/js?id=UA-88778470-4"></NextScript>
           <NextScript
@@ -31,11 +30,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <NextScript defer strategy="lazyOnload" rel="prefetch" src="clippyjs/build/clippy.min.js"></NextScript>
-          <link defer rel="prefetch" as="style" type="text/css" href="clippyjs/build/clippy.css" />
-          {/* <NextScript defer strategy="lazyOnload" rel="prefetch" src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="></NextScript> */}
-          {/* <NextScript defer strategy="lazyOnload" rel="prefetch" src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></NextScript>
-          <link defer rel="prefetch" as="style" type="text/css" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css" /> */}
         </body>
       </Html>
     )
