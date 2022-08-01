8ef35d85ccb46cf6d8fe85b093b438af622fea31
Nhu Lam <nhu@sanctuary.computer>
2022-08-01 12:34:23 -0400

    changing prop types

--- components/WorkSectionAsGallery.js
+++ components/WorkSectionAsGallery.js
@@ -1,115 +1,13 @@
-import React, { useRef, useState, useEffect } from 'react';
-
-import Link from 'next/link';
+import React from 'react';
 
 import PropTypes from 'prop-types';
 
 import { ContentfulMedia, SimpleFragment } from 'models';
 
-import { Markdown, Image } from 'components/base';
+import { Markdown } from 'components/base';
+import Work from 'components/Work';
 
 import get from 'utils/get';
-import flattenImageData from 'utils/flattenImageData';
-
-const PLAY_VIDEO = "(Play)";
-const PAUSE_VIDEO = "(Pause)"; 
-
-const renderWork = (work) => {
-  const [videoStatus, setVideoStatus] = useState(PLAY_VIDEO);
-  const videoRef = useRef(null);
-  const buttonRef = useRef(null); 
-
-  useEffect(() => {
-    if (videoStatus === PLAY_VIDEO) {
-      videoRef.current?.play(); 
-      buttonRef.current?.textContent = PAUSE_VIDEO;
-      buttonRef.current?.ariaLabel = `pause video for ${title}`
-
-    } else {
-      videoRef.current?.pause();
-      buttonRef.current?.textContent = PLAY_VIDEO;
-      buttonRef.current?.ariaLabel = `play video for ${title}`
-    }
-  }, [videoStatus])
-
-  const workIsImage = get(work, 'fields.asset.fields.file.contentType', '').startsWith("image/");  
-  const workImage = flattenImageData(get(work, 'fields.asset', {}));
-  const title = get(work, 'fields.title', '');
-  const caseStudySlug = get(work, 'fields.caseStudySlug', '');
-  const link = get(work, 'fields.link', '');
-  const src = get(work, 'fields.video.fields.file.url', '');
-  const id = get(work, 'sys.id', '');
-
- return (
-   <>
-    <div className="WorkSectionAsGallery__work-hover-overlay flex justify-between items-end color-white absolute h100 w100">
-      <div className="WorkSectionAsGallery__work-hover-overlay--info flex flex-col justify-evenly mb_5 ml_5">
-        <div className="WorkSectionAsGallery__work-hover-overlay--title">{title}</div>
-        {caseStudySlug ? 
-          <Link
-            href={caseStudySlug}
-          >
-            <a
-              className="decoration-none color-white"
-              aria-label={`read the case study for ${title}`}
-              rel="noopener noreferrer"
-            >
-            (read case study)
-            </a>
-          </Link> : 
-          <Link
-            href={link}
-          >
-            <a
-              className="decoration-none color-white"
-              aria-label={`visit the site for ${title}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            → visit the site 
-            </a>
-          </Link>
-        }
-      </div>
-      {!workIsImage &&
-        <button 
-          className="WorkSectionAsGallery__work-hover-overlay--video-button"
-          aria-label={`pause video for ${title}`}
-          ref={buttonRef}
-          onClick={() => {
-            videoStatus === PAUSE_VIDEO ? setVideoStatus(PLAY_VIDEO) : setVideoStatus(PAUSE_VIDEO)
-          }}
-        >
-          (Pause)
-        </button>
-      }
-    </div>
-    {workIsImage ? 
-      <Image
-        src={workImage.url}
-        width={workImage.width}
-        height={workImage.height}
-        sizes='50vw'
-      />  :
-      <video
-        id={id}
-        key={id}
-        style={{
-          width: '100%', 
-          height: '100%' 
-        }}
-        autoPlay
-        loop
-        muted
-        playsInline
-        ref={videoRef}
-      >
-        <source src={src}></source>
-      </video>
-    }
-    </>
- );
-}
 
 const renderWorkGalleryAssetBlock = (workGalleryBlock) => {
   const displayFirstAssetAsFullWidth = get(workGalleryBlock, 'fields.displayFirstAssetAsFullWidth', true);
@@ -120,17 +18,17 @@ const renderWorkGalleryAssetBlock = (workGalleryBlock) => {
       {displayFirstAssetAsFullWidth && 
         <>
           <div className="block col-8 relative">
-            {renderWork(works[0])}
+            <Work work={works[0]}/>
           </div>
           <div className="flex col-8 pb_5 md:pb1 items-end">
             <div className="block col-4 pr_25 md:pr_5">
               <div className="relative">
-                {renderWork(works[1])}
+                <Work work={works[1]}/>
               </div>
             </div>
             <div className="block col-4 pr_25 md:pr_5">
               <div className="relative">
-                {renderWork(works[2])}
+                <Work work={works[2]}/>
               </div>
             </div>
           </div>
@@ -141,17 +39,17 @@ const renderWorkGalleryAssetBlock = (workGalleryBlock) => {
           <div className="flex col-8 pb_5 md:pb1 items-end">
             <div className="block col-4 pr_25 md:pr_5">
               <div className="relative">
-                {renderWork(works[0])}
+                <Work work={works[0]}/>
               </div>
             </div>
             <div className="block col-4 pr_25 md:pr_5">
               <div className="relative">
-                {renderWork(works[1])}
+                <Work work={works[1]}/>
               </div>
             </div>
           </div>
           <div className="block col-8 relative">
-            {renderWork(works[2])}
+            <Work work={works[2]}/>
           </div>
         </>
       }
@@ -205,27 +103,28 @@ const WorkPropType = PropTypes.shape({
     link: PropTypes.string,
     linkLabel: PropTypes.string,
     stack: SimpleFragment,
-    collaborators: SimpleFragment
+    collaborators: SimpleFragment,
+    video: ContentfulMedia
   })
 }); 
 
-const WorkGalleryBlockPropType = PropTypes.shape({
+const WorkGalleryTextBlockPropType = PropTypes.shape({
+  text: PropTypes.string
+})
+
+const WorkGalleryAssetBlockPropType = PropTypes.shape({
   title: PropTypes.string, 
-  fullWidthWork: WorkPropType,
-  halfWidthWorkOne: WorkPropType,
-  halfWidthWorkTwo: WorkPropType,
-  displayFullWidthAssetAtTop: PropTypes.bool
-});
+  works: PropTypes.arrayOf(WorkPropType),
+  displayFirstAssetAsFullWidth: PropTypes.bool
+})
 
 WorkSectionAsGallery.propTypes = {
   workGallery: PropTypes.shape({
     fields: PropTypes.shape({
       title: PropTypes.string, 
-      workGalleryBlockOne: WorkGalleryBlockPropType,
-      workGalleryTextOne: PropTypes.string,
-      workGalleryBlockTwo: WorkGalleryBlockPropType,
-      workGalleryTextTwo: PropTypes.string,
-      workGalleryBlockAnyRemaining: PropTypes.arrayOf(WorkGalleryBlockPropType)
+      workGalleryAssetBlocks: PropTypes.arrayOf(WorkGalleryAssetBlockPropType),
+      workGalleryTextBlocks: PropTypes.arrayOf(WorkGalleryTextBlockPropType),
+      renderAssetBlocksFirst: PropTypes.bool
     })
   })
 }
