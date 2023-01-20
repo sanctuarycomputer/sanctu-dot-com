cf34c5bd3fb15b948f4bad23db406aee5dd61c3f
NAHEE KIM <naheekim@nahees-air.mynetworksettings.com>
2023-01-20 14:43:07 -0500

    Manually trigger preview deployment

--- components/BlockGeneralInfo.js
+++ components/BlockGeneralInfo.js
@@ -6,6 +6,7 @@ import simpleFragmentToListItems from 'utils/simpleFragmentToListItems';
 import cx from 'classnames';
 
 import { SimpleFragment } from 'models';
+
 import { List, Markdown } from 'components/base';
 
 const BlockGeneralInfo = (props) => {
