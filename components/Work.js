4d5cc74d0818433b613d9ab2c2f13481dcdd246c
NAHEE KIM <naheekim@nahees-air.mynetworksettings.com>
2023-01-25 11:09:08 -0500

    fix mis alignment in image grid

--- components/Work.js
+++ components/Work.js
@@ -11,41 +11,45 @@ import { Image } from 'components/base';
 import get from 'utils/get';
 import flattenImageData from 'utils/flattenImageData';
 
-const PLAY_VIDEO = "(Play)";
-const PAUSE_VIDEO = "(Pause)"; 
+const PLAY_VIDEO = '(Play)';
+const PAUSE_VIDEO = '(Pause)';
 
 const Work = ({ work, width = '100vw' }) => {
   const [videoStatus, setVideoStatus] = useState(PLAY_VIDEO);
   const videoRef = useRef(null);
-  const buttonRef = useRef(null); 
+  const buttonRef = useRef(null);
 
   useEffect(() => {
-    if (videoStatus == PLAY_VIDEO) { 
-      videoRef.current?.play(); 
+    if (videoStatus == PLAY_VIDEO) {
+      videoRef.current?.play();
 
       const button = buttonRef.current;
       if (button) {
         button.textContent = PAUSE_VIDEO;
-        button.ariaLabel = `pause video for ${title}`
+        button.ariaLabel = `pause video for ${title}`;
       }
-    } else { 
+    } else {
       videoRef.current?.pause();
 
       const button = buttonRef.current;
-      if(button) {
+      if (button) {
         button.textContent = PLAY_VIDEO;
-        button.ariaLabel = `play video for ${title}`
+        button.ariaLabel = `play video for ${title}`;
       }
     }
-  }, [videoStatus])
+  }, [videoStatus]);
 
   const workIsPlaceholder = !work?.fields?.name;
 
-  if (!work || workIsPlaceholder) { 
-    return null; 
+  if (!work || workIsPlaceholder) {
+    return null;
   }
 
-  const workIsImage = get(work, 'fields.asset.fields.file.contentType', '').startsWith("image/");  
+  const workIsImage = get(
+    work,
+    'fields.asset.fields.file.contentType',
+    ''
+  ).startsWith('image/');
   const workImage = flattenImageData(get(work, 'fields.asset', {}));
   const title = get(work, 'fields.title', '');
   const caseStudySlug = get(work, 'fields.caseStudySlug', '');
@@ -54,73 +58,81 @@ const Work = ({ work, width = '100vw' }) => {
   const id = get(work, 'sys.id', '');
 
   return (
-   <>
-    <div className={`WorkSectionAsGallery__work-hover-overlay ${workIsImage ? 'WorkSectionAsGallery__work-hover-overlay--for-image' : 'WorkSectionAsGallery__work-hover-overlay--for-video'} flex justify-between items-end color-white absolute`}>
-      <div className="WorkSectionAsGallery__work-hover-overlay--info flex flex-col justify-evenly mb_5 ml_5">
-        <div className="WorkSectionAsGallery__work-hover-overlay--title">{title}</div>
-        {caseStudySlug ? 
-          <Link
-            href={caseStudySlug}
-          >
-            <a
-              className="decoration-none color-white"
-              aria-label={`read the case study for ${title}`}
-              rel="noopener noreferrer"
-            >
-            (read case study)
-            </a>
-          </Link> : 
-          <Link
-            href={link}
-          >
-            <a
-              className="decoration-none color-white"
-              aria-label={`visit the site for ${title}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            → visit the site 
-            </a>
-          </Link>
-        }
-      </div>
-      {!workIsImage &&
-        <button 
-          className="WorkSectionAsGallery__work-hover-overlay--video-button"
-          aria-label={`pause video for ${title}`}
-          ref={buttonRef}
-          onClick={() => {
-            videoStatus === PAUSE_VIDEO ? setVideoStatus(PLAY_VIDEO) : setVideoStatus(PAUSE_VIDEO)
-          }}
-        >
-          (Pause)
-        </button>
-      }
-    </div>
-    {workIsImage ? 
-      <Image
-        alt={workImage.alt}
-        src={workImage.url}
-        width={workImage.width}
-        height={workImage.height}
-        sizes={width}
-      />  :
-      <video
-        id={id}
-        key={id}
-        className="w100 h100"
-        autoPlay
-        loop
-        muted
-        playsInline
-        ref={videoRef}
+    <>
+      <div
+        className={`WorkSectionAsGallery__work-hover-overlay ${
+          workIsImage
+            ? 'WorkSectionAsGallery__work-hover-overlay--for-image'
+            : 'WorkSectionAsGallery__work-hover-overlay--for-video'
+        } flex justify-between items-end color-white absolute`}
       >
-        <source src={src}></source>
-      </video>
-    }
+        <div className="WorkSectionAsGallery__work-hover-overlay--info flex flex-col justify-evenly mb_5 ml_5">
+          <div className="WorkSectionAsGallery__work-hover-overlay--title">
+            {title}
+          </div>
+          {caseStudySlug ? (
+            <Link href={caseStudySlug}>
+              <a
+                className="decoration-none color-white"
+                aria-label={`read the case study for ${title}`}
+                rel="noopener noreferrer"
+              >
+                (read case study)
+              </a>
+            </Link>
+          ) : (
+            <Link href={link}>
+              <a
+                className="decoration-none color-white"
+                aria-label={`visit the site for ${title}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                → visit the site
+              </a>
+            </Link>
+          )}
+        </div>
+        {!workIsImage && (
+          <button
+            className="WorkSectionAsGallery__work-hover-overlay--video-button"
+            aria-label={`pause video for ${title}`}
+            ref={buttonRef}
+            onClick={() => {
+              videoStatus === PAUSE_VIDEO
+                ? setVideoStatus(PLAY_VIDEO)
+                : setVideoStatus(PAUSE_VIDEO);
+            }}
+          >
+            (Pause)
+          </button>
+        )}
+      </div>
+      {workIsImage ? (
+        <Image
+          alt={workImage.alt}
+          src={workImage.url}
+          width={workImage.width}
+          height={workImage.height}
+          sizes={width}
+        />
+      ) : (
+        <video
+          id={id}
+          key={id}
+          className="block w100 h100"
+          autoPlay
+          loop
+          muted
+          playsInline
+          ref={videoRef}
+        >
+          <source src={src}></source>
+        </video>
+      )}
     </>
- );
-}
+  );
+};
 
 Work.propTypes = {
   work: PropTypes.shape({
@@ -132,10 +144,9 @@ Work.propTypes = {
       linkLabel: PropTypes.string,
       stack: SimpleFragment,
       collaborators: SimpleFragment,
-      video: ContentfulMedia
-    })
-  })
-}
+      video: ContentfulMedia,
+    }),
+  }),
+};
 
-export default Work; 
-
+export default Work;
