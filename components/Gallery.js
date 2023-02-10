5d0c6830bf77c0237adecaca94082340bdefc0ef
nahbee10 <nahee@sanctuary.computer>
2023-02-10 09:52:45 -0500

    add overflow style only to x axis

--- components/Gallery.js
+++ components/Gallery.js
@@ -92,7 +92,7 @@ class Gallery extends PureComponent {
     );
 
     return (
-      <div className="col-8 p1 flex flex-wrap">
+      <div className="Gallery col-8 p1 flex flex-wrap">
         {imageMatrix.map((imageGroup, index) =>
           this.renderGalleryRow(imageGroup, index, imageMatrix)
         )}
