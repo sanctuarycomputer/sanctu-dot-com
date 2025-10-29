5d934742fa42b3e8d1bfd1eb97587a29b88f1084
Hugh Francis <hughfrancis89@gmail.com>
2025-10-29 12:51:36 -0400

    Add apollo visitor tracking

--- pages/_app.js
+++ pages/_app.js
@@ -70,6 +70,12 @@ function MyApp({ Component, pageProps }) {
           gtag('js', new Date());
           gtag('config', 'UA-88778470-4');`}
       </Script>
+      <Script id="apollo-visitors">
+        {`function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"664399ea265b7701c790c997"})},
+document.head.appendChild(o)}initApollo();`}
+      </Script>
       <Script id="hotjar">
         {`(function(h,o,t,j,a,r){
           h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
